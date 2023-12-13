f32e3714d25fc36d7b054eeeed1eec0349cbad11
Kgaugelo-Shai <144380460+Kgaugelo-Shai@users.noreply.github.com>
2023-12-13 20:28:36 +0200

    Working shell 2

--- usr_cmd.c
+++ usr_cmd.c
@@ -1,6 +1,54 @@
 #include "shell.h"
 
 /**
+ * init_sh - runs the shell in interactive mode
+ *
+ * Returb: void
+ */
+void init_sh(void)
+{
+	char *cmd = NULL;
+	size_t _buf = 0;
+
+	if (isatty(fileno(stdin)))
+	{
+		while (1)
+		{
+			prmpt();
+			if (getline(&cmd, &_buf, stdin) == -1)
+			{
+				printf("\n");
+				break;
+			}
+			if (_strlen(cmd) <= 1)
+				continue;
+			if (exec_cmd(cmd) == -1)
+			{
+				free(cmd);
+				perror(cmd);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	else
+	{
+		if (getline(&cmd, &_buf, stdin) == -1)
+		{
+			perror(cmd);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			if (exec_cmd(cmd) != 0)
+			{
+				perror("Error");
+				free(cmd);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+/**
  * usr_cmd - receives the user input from stdin
  * @argc: argument counter
  * @argv: pointer to array of arguments
@@ -11,12 +59,11 @@ void usr_cmd(int argc, char *argv[])
 {
 	if (argc > 1)
 	{
-		int arg_cnt, fd;
+		int arg_cnt;
 
 		for (arg_cnt = 1; arg_cnt < argc; arg_cnt++)
 		{
-			fd = exec_cmd(argv[arg_cnt]);
-			if (fd != 0)
+			if (exec_cmd(argv[arg_cnt]) != 0)
 			{
 				perror("Error");
 				exit(EXIT_FAILURE);
@@ -25,45 +72,6 @@ void usr_cmd(int argc, char *argv[])
 	}
 	else
 	{
-		char *cmd = NULL;
-		size_t _buf = 0;
-
-		if (isatty(fileno(stdin)))
-		{
-			while (1)
-			{
-				prmpt();
-				if (getline(&cmd, &_buf, stdin) == -1)
-				{
-					printf("\n");
-					break;
-				}
-				if (_strlen(cmd) <= 1)
-					continue;
-				if (exec_cmd(cmd) == -1)
-				{
-					free(cmd);
-					perror(cmd);
-					exit(EXIT_FAILURE);
-				}
-			}
-		}
-		else
-		{
-			if (getline(&cmd, &_buf, stdin) == -1)
-			{
-				perror(cmd);
-				exit(EXIT_FAILURE);
-			}
-			else
-			{
-				if (exec_cmd(cmd) != 0)
-				{
-					perror("Error");
-					free(cmd);
-					exit(EXIT_FAILURE);
-				}
-			}
-		}
+		init_sh();
 	}
 }
